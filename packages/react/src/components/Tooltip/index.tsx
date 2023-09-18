b92f32a0c8a6332dc7dd11c6d9d3b340e3044b0c
Renato Xavier <rnatu91@gmail.com>
2023-09-18 12:58:03 -0300

    feat: add Tooltip stories withou styling with css

---
+++ packages/react/src/components/Tooltip/index.tsx
@@ -0,0 +1,27 @@
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import { ComponentProps, ReactNode } from 'react';
+
+export interface TooltipProps extends ComponentProps<typeof TooltipPrimitive.Root> {
+  content: string | ReactNode;
+}
+
+export function Tooltip({ children, content, ...props }: TooltipProps) {
+  return (
+    <TooltipPrimitive.Provider>
+      <TooltipPrimitive.Root {...props}>
+        <TooltipPrimitive.Trigger asChild>
+        {children}
+        </TooltipPrimitive.Trigger>
+        
+        <TooltipPrimitive.Portal>
+          <TooltipPrimitive.Content>
+            <TooltipPrimitive.Arrow />
+
+            {content}
+          </TooltipPrimitive.Content>
+        </TooltipPrimitive.Portal>
+      </TooltipPrimitive.Root>
+    </TooltipPrimitive.Provider>
+  )
+}
+
